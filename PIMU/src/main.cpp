f947275b26f57e7607c46138bff4345123ac2b62
Owen H. <owenjh12345@gmail.com>
2026-01-10 15:52:01 -0800

    BME280 and MPRLS sensors

--- PIMU/src/main.cpp
+++ PIMU/src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "defines.h"
 #include "sensors/IMU.h"
+#include "sensors/Pressure.h"
 
 /// If the serial did not connect.
 bool faultySerial = false;
@@ -9,8 +10,9 @@ bool faultyIMU = false;
 
 /// The IMU connection.
 IMU imu;
+Pressure pressure = Pressure();
 
-void setup() 
+void setup()
 {
     // Setup serial.
     if (setup_serial()) 
@@ -19,7 +21,17 @@ void setup()
         SerialUSB.println("Connected to the serial.");
     }
     
-    // Setup the IMU.
+    if (pressure.connect_to_sensor()) 
+    {
+        SerialUSB.println("Pressure sensor connected!");
+        pressure.configure_sensor();
+    }
+    else
+    {
+        SerialUSB.println("Pressure not connected. Moving on without pressure :(");
+    }
+    
+    //Setup the IMU.
     if (imu.connect_to_imu())
     {
         SerialUSB.println("IMU connected!!");
@@ -34,6 +46,7 @@ void setup()
 void loop() 
 {
     imu.imu_loop();
+    pressure.sensor_loop();
     delay(LOOP_DELAY);
 }
 
@@ -43,7 +56,7 @@ void loop()
 bool setup_serial() 
 {
     // Try to connect to USB Serial.
-    SerialUSB.begin(BAUD);
+    SerialUSB.begin(BAUD_RATE);
     
     // Prevent an infinate loop by setting an upper bound.
     int safetyCount = 0;
---
+++ PIMU/src/sensors/Pressure.cpp
@@ -0,0 +1,45 @@
+#include "sensors/Pressure.h"
+
+
+bool Pressure::connect_to_sensor(int retryDefault)
+{
+    // Connect to the IMU.
+    int safetyCount = retryDefault;
+    while (!pressure.begin(119U, &PIMU_WIRE) && safetyCount < RETRY_MAX) 
+    {
+        SerialUSB.println("Cannot connect to Pressure! Retrying...");
+        safetyCount++;
+        delay(100);
+    }
+    
+    // Check for IMU
+    pressureConnected = safetyCount < RETRY_MAX;
+    return pressureConnected;
+}
+
+void Pressure::configure_sensor() 
+{
+    
+}
+
+Pressure::Pressure() 
+{
+    pressure = Adafruit_BME280();
+}
+
+void Pressure::sensor_loop() 
+{
+    if (pressureConnected) 
+    {        
+        // in hPa
+        float pressureReading = pressure.readPressure();
+        SerialUSB.printf("Pressure: %f Pa\n", pressureReading);
+    }
+    else 
+    {
+        // Try to connect once more while looping to try and salvage the IMU.
+        SerialUSB.print("Attempting to connect to pressure in main loop...");
+        // Pass in RETRY_MAX - 1 to only try once.
+        connect_to_sensor(RETRY_MAX - 1);
+    }
+}
